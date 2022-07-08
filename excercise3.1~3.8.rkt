18b05413c80eaf759b74f634167d4e100fb1e59b
memorelic <zk19980223@outlook.com>
2022-07-08 18:34:22 +0800

    finish excercise 3.8, but remain some questions about Sameness and change.

--- excercise3.1~3.8.rkt
+++ excercise3.1~3.8.rkt
@@ -43,13 +43,13 @@
 ;; f on that input and increments the counter. For instance, we could
 ;; make a monitored version of the sqrt procedure:
 
- (define (make-monitored f) 
-   (let ((count 0)) 
-     (lambda (arg) 
-       (cond [(eq? arg 'how-many-calls?) count] 
-             [(eq? arg 'reset-count) (set! count 0)] 
-             [else (begin (set! count (+ count 1)) 
-                          (f arg))])))) 
+(define (make-monitored f) 
+  (let ((count 0)) 
+    (lambda (arg) 
+      (cond [(eq? arg 'how-many-calls?) count] 
+            [(eq? arg 'reset-count) (set! count 0)] 
+            [else (begin (set! count (+ count 1)) 
+                         (f arg))])))) 
           
   
 ;; Excercise 3.3:
@@ -87,7 +87,7 @@
           "Insufficient funds"))
 
     (define (deposit amount)
-          (set! blance (+ blance amount)))
+      (set! blance (+ blance amount)))
 
     (define (password-match? given-password)                         
       (eq? given-password password))                          
@@ -114,7 +114,7 @@
     dispatch))
 
 (define (call-the-cops)
-    (error "You try too much times, calling the cops ..."))
+  (error "You try too much times, calling the cops ..."))
 
 ;; Excercise 3.5:
 ;; Monte Carlo integration is a method of estimating
@@ -167,8 +167,8 @@
 (define (estimate-integral p? x1 x2 y1 y2 trials)
   (* (rect-area x1 x2 y1 y2)
      (monte-carlo trials
-                 (lambda () (p? (random-in-range x1 x2)
-                                (random-in-range y1 y2))))))
+                  (lambda () (p? (random-in-range x1 x2)
+                                 (random-in-range y1 y2))))))
 
 (define (get-pi trials)
   (exact->inexact
@@ -201,3 +201,51 @@
   (if (<= times 0)
       '()
       (cons (rand 'generate) (rand-enumerate (- times 1)))))
+
+;; Excercise 3.7:
+;; Consider the bank account objects created by
+;; make-account, with the password modification described in
+;; Exercise 3.3. Suppose that our banking system requires the ability
+;; to make joint accounts. Define a procedure make-joint that
+;; accomplishes this. Make-joint should take three arguments. The
+;; first is a password-protected account. The second argument must
+;; match the password with which the account was defined in order
+;; for the make-joint operation to proceed. The third argument
+;; is a new password. Make-joint is to create an additional access
+;; to the original account using the new password. For example, if
+;; peter-acc is a bank account with password open-sesame, then
+
+;(define paul-acc
+;  (make-joint peter-acc 'open-sesame 'rosebud))
+
+;; will allow one to make transactions on peter-acc using the name
+;; paul-acc and the password rosebud. You may wish to modify your
+;; solution to Exercise 3.3 to accommodate this new feature.
+
+(define (make-joint origin-acc origin-passwd another-passwd)
+  (lambda (given-passwd mode)
+    (if (eq? given-passwd another-passwd)
+        (origin-acc origin-passwd mode)
+        display-wrong-another-password-message)))
+
+(define (display-wrong-another-password-message useless-arg)
+  (display "Incorrect password"))
+
+;; Excercise 3.8:
+;; When we defined the evaluation model in Section
+;; 1.1.3, we said that the first step in evaluating an expression is to
+;; evaluate its subexpressions. But we never specified the order in
+;; which the subexpressions should be evaluated (e.g., left to right or
+;; right to left). When we introduce assignment, the order in which
+;; the arguments to a procedure are evaluated can make a difference
+;; to the result. Define a simple procedure f such that evaluating
+
+; (+ (f 0) (f 1))
+
+;; will return 0 if the arguments to + are evaluated from left to right
+;; but will return 1 if the arguments are evaluated from right to left.
+
+;; This solution can't work under #lang sicp, but it does work under #lang racket
+(define f
+  (let ((y 1))
+    (lambda (x) (set! y (* x y)) y)))
