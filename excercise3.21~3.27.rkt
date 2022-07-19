7fc925d224efa0dcb8a9ffa5c5111fbcb18dad57
memorelic <zk19980223@outlook.com>
2022-07-19 18:39:52 +0800

    finish excercise 3.23.

--- excercise3.21~3.27.rkt
+++ excercise3.21~3.27.rkt
@@ -126,7 +126,66 @@
 ;; represent deques using pairs, and give implementations of the
 ;; operations.23 All operations should be accomplished in O(1) steps.
 
+;; ptr setter
+
+
+
 (define (make-deque)
   (cons '() '()))
 
-(define 
+(define (empty-deque? deque)
+  (null? (front-ptr deque)))
+
+(define (front-deque deque)
+  (if (empty-deque? deque)
+      (error "FRONT-DEQUE called with an empty deque" deque)
+      (car (front-ptr deque))))
+
+(define (rear-deque deque)
+    (if (empty-deque? deque)
+        (error "REAR-DEQUE called with an empty deque" deque)
+        (car (rear-ptr deque))))
+
+(define (insert-rear-deque! deque item)
+  (let ((new-pair (cons item '())))
+    (cond [(empty-deque? deque)
+           (set-front-ptr! deque new-pair)
+           (set-rear-ptr! deque new-pair)
+           deque]
+          [else
+           (set-cdr! (rear-ptr deque) new-pair)
+           (set-rear-ptr! deque new-pair)
+           deque])))
+
+(define (delete-front-deque! deque)
+  (cond [(empty-deque? deque)
+         (error "DELETE-FRONT-DEQUE! called with an empty deque" deque)]
+        [else
+         (set-front-ptr! deque (cdr (front-ptr deque)))
+         deque]))
+
+(define (insert-front-deque! deque item)
+  (cond [(empty-deque? deque)
+         (insert-rear-deque! deque item)]
+        [else
+         (set-front-ptr! deque (cons item (front-ptr deque)))
+         deque]))
+
+(define (delete-rear-deque! deque)
+  (define (iter deque lst)
+    (cond [(null? (cdr (cdr lst)))
+           (set-cdr! lst '())
+           (set-rear-ptr! deque lst)
+           deque]
+          [else
+           (iter deque (cdr lst))]))
+  (cond [(empty-deque? deque)
+         (error "DELETE-REAR-DEQUE! called with an empty deque" deque)]
+        [(null? (cdr (front-ptr deque))) ; 长度为1时
+         (set-front-ptr! deque '())
+         deque]
+        [else
+         (iter deque (front-ptr deque))])) ; 长度大于1时
+
+(define (print-deque deque)
+    (car deque))
